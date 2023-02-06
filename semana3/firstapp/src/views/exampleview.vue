cb4f446e60f7c11200daaddda0ce4984f1a2b35b
SU-NOMBRE-DE-USUARIO-DE-GITUHUB <correostudent12@gmail.com>
2023-02-05 18:15:20 -0600

    Aqui agrego la practica de la semana 3

---
+++ semana3/firstapp/src/views/exampleview.vue
@@ -0,0 +1,14 @@
+<template>
+    <examplecomponent title="hi"></examplecomponent>
+</template>
+
+<script>
+import examplecomponent from '@/components/examplecomponent.vue';
+
+export default {
+    name: 'example',
+    components: {
+        examplecomponent,
+    }
+}
+</script>
